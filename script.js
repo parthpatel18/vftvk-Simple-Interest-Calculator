aff6abd5aa7ab2a781460a263eb70a4e4b6c3115
Parth Patel <parthpatel199618@gmail.com>
2021-07-09 05:43:57 +0000

    Final commit

--- script.js
+++ script.js
@@ -9,12 +9,20 @@ function compute()
 
     var year = new Date().getFullYear() + parseInt(years);
 
+    if(principal <= 0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+
     document.getElementById("result").innerHTML = 'If you deposit <mark>' +  principal + '</mark>,</br> at an interest rate of <mark>' + rate + '</mark>.</br> You will receive an amount of <mark>' + interest + '</mark>,</br> in the year <mark>' + year + '</mark>';
+
+    return true;
 }
 
 //Displays the value of rate slider
 function updateRate(){
     var rateval = document.getElementById("rate").value;
 
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerHTML = rateval;
 }
